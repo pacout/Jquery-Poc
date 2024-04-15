fee3013a8700ba25cfa28a8cc09e78f574291f63
Franc Westphal <F@Zettanet.com>
2024-04-15 15:08:47 -0600

    Initial Commit

---
+++ src/build/mainController.js
@@ -0,0 +1,7 @@
+"use strict";
+function returnVal() {
+    alert("Hey There Courtney");
+}
+function thinger(someVal) {
+    alert("Your Val: " + someVal);
+}
---
+++ src/controller/mainController.ts
@@ -0,0 +1,8 @@
+function returnVal(){
+   alert("Hey There Courtney");
+}
+
+function thinger(someVal:string){
+   alert("Your Val: " + someVal);
+}
+
